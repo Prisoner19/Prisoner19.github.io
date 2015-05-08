67012627cd8d4c2fdca478bf1ea3d2b8d303c5ba
Germán Cruz <clz.german@gmail.com>
2015-05-08 03:52:26 -0500

    new

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@
      setTimeout(function(){document.getElementById("welcome").innerHTML = "Bienvenido, Gonzalo."; 
                           setTimeout(function(){window.location.href ='menu.html'},500)},500);
 });
+
+ $("#logout-button").click(function(event){
+     event.preventDefault();
+	 document.getElementById("welcome-text-header").innerHTML = "Hasta pronto.";
+     setTimeout(function(){window.location.href ='index.html'},500);
+});
